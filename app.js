e98499c1946591e018791709ece61be63c10f6d7
Mikael <dev.kov.mtl@gmail.com>
2022-03-11 20:34:41 -0500

    Added catalog router and dummy controller

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOST, MONGO_DATABASE } =
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog');
 
 const MONGO_OPTIONS = {
   useNewUrlParser: true,
@@ -40,6 +41,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
