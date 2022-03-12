4c378375c1b445c56862b37180f374c0bb5d30fa
Mikael <dev.kov.mtl@gmail.com>
2022-03-12 15:37:06 -0500

    Author controller and view

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const { DateTime } = require('luxon');
 
 const AuthorSchema = new Schema({
   first_name: { type: String, required: true, maxlength: 100 },
@@ -37,6 +38,18 @@ AuthorSchema.virtual('lifespan').get(function () {
 // Virtual for author url
 AuthorSchema.virtual('url').get(function () {
   return '/catalog/author' + this._id;
+});
+
+AuthorSchema.virtual('date_of_birth_formatted').get(function () {
+  return this.date_of_birth
+    ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
+    : '';
+});
+
+AuthorSchema.virtual('date_of_death_formatted').get(function () {
+  return this.date_of_death
+    ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+    : '';
 });
 
 module.exports = model('Author', AuthorSchema);
